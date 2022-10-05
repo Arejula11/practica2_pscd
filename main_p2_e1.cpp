0e838ada1725421fd394eebfb1bdd57fd585bf18
Miguel Aréjula Aisa <arejula10@gmail.com>
2022-10-05 23:28:38 +0200

    coms e1 y avance e2

--- main_p2_e1.cpp
+++ main_p2_e1.cpp
@@ -1,12 +1,14 @@
 /* ----------------------------------------------------------------------------
 * File:   practica_1_V1.cpp
 * Author: PSCD-Unizar
-* Date:   septiembre 2020
+* Date:   octubre 2022
 * Coms:   Parte del práctica 1 de PSCD
 *         Compilar mediante
 *           g++ practica_1_V1.cpp -o practica_1_V1 -std=c++11 -pthread
 * ----------------------------------------------------------------------------- */
 //1 23 veces y 4 15 veces
+
+
 #include <iostream>
 #include <thread>
 #include <string>
@@ -21,51 +23,61 @@ using VectInt = int[N];
 const int N_INTERVALOS = (N)/N_BUSC;
 
 
-
+//-----------------------------------------------------
+//Pre: v es un vector de N de enteros  
+//Post: v es completado con los números contenidos en el fichero "datos.txt"
 void leerFichero(VectInt v){
     ifstream f("datos.txt");
     if(f.is_open()){
-        for(int i=0; i<N; i++){
+        for(int i = 0; i < N; i++){
             f >> v[i];
         }
     }else{
-        cout << "error" << endl;
+        cout << "error" << endl; // en caso de no completar el vector "v" muestra en pantalla un mensaje de error
     }
 }
 
-
+//-----------------------------------------------------
+//Pre: 0 <= i <= d <= N−1
+//Post: result = Num α ∈ [i, d].value = v[α]
 void search(const VectInt v, const int i, const int d, const int value, int& result){
-    for(int j=i; j <= d; j++){
+    for(int j = i; j <= d; j++){
         if(v[j]==value){
-            result++;
+            result++; // al ser una variable local para cada proceso se puede ejecutar la suma sin posibles errores
         }
     }
 }
 
+//-----------------------------------------------------
+//Pre: 
+//Post: 
 void coordinador(bool& comenzar, VectInt v, bool fin_procesos[], int contador[]){
     leerFichero(v);
-    comenzar = true;
-    for(int i=0; i<N_BUSC; i++){
-        while(!fin_procesos[i]){
+    comenzar = true; //Avisa a los procesos buscadores de la carga de datos
+    for(int i = 0; i < N_BUSC; i++){
+        while(!fin_procesos[i]){ //espera activa, hasta la finalización de los procesos buscadores
         }
     }
-    int suma=0;
-    for(int i=0; i < N_BUSC; i++){
-        suma+=contador[i];
+    int suma = 0;
+    for(int i = 0; i < N_BUSC; i++){
+        suma += contador[i]; //suma las componentes del vector rellenado por los procesos buscadores
     }
-    cout << "Resultado: " +to_string(suma) + '\n';
+    cout << "Resultado: " + to_string(suma) + '\n';
     
 }
 
+//-----------------------------------------------------
+//Pre: 
+//Post: 
 void buscador(bool& comenzar, const VectInt v, int i, int value, int contador[], bool fin_procesos[]){
     while(!comenzar){
 
     }
     int result = 0;
     search(v,(i)*N_INTERVALOS, (i+1)*N_INTERVALOS-1, value, result);
-    contador[i]= result;
-    fin_procesos[i]= true;
-    //cout<<"FIN PROCESO: "+ to_string(i) + '\n';
+    contador[i] = result;
+    fin_procesos[i] = true;
+    
 }
 
 
@@ -75,35 +87,34 @@ void buscador(bool& comenzar, const VectInt v, int i, int value, int contador[],
 int main(){
     bool comenzar = false;
     int contador[N_BUSC];
-    bool fin_procesos[N_BUSC]={false};
+    bool fin_procesos[N_BUSC] = {false};
     VectInt v;
     bool valCorrec = false;
     int value;
-    cout<<"Introduce un valor entre 1 y 25: "<<endl;
-    cin>>value;
-    do {
-        if(value<1 || value >25){
-        cout<< "El valor no se encuentra entre los valores indicados"<<endl;
-        cout<< "Vuelve a introudcir un valor:" <<endl;
-        cin>>value;
+    
+    cout << "Introduce un valor entre 1 y 25: "<<endl;
+    cin >> value;
+    do { //comprueba que el valor introducido se encuentra entre el 1 y el 25
+        if(value < 1 || value > 25){
+        cout << "El valor no se encuentra entre los valores indicados"<< endl;
+        cout << "Vuelve a introudcir un valor:" << endl;
+        cin >> value;
         }else{
             valCorrec = true;
         }
     }while(!valCorrec);
 
-    thread P[N_BUSC+1];
-    P[N_BUSC]= thread(&coordinador, ref(comenzar), ref(v), ref(fin_procesos), ref(contador));
-    for (int i = 0; i< N_BUSC; i++){
-        P[i]= thread(&buscador,ref(comenzar), v, i, value,contador,fin_procesos);
+    thread P[N_BUSC+1]; //declaración procesos sin poner en marcha
+    P[N_BUSC]= thread(&coordinador, ref(comenzar), ref(v), ref(fin_procesos), ref(contador)); // el proceso coordinador se pone en marcha
+    for (int i = 0; i < N_BUSC; i++){
+        P[i]= thread(&buscador,ref(comenzar), v, i, value,contador,fin_procesos); // los procesos buscadores se ponen en marcha
     }
     
    for (int i = 0; i < N_BUSC+1; i++){
          P[i].join();
          
     }
-    cout<<"FIN";
-    /*for(int i = 0; i<N_BUSC;i++){
-        cout<<contador[i]<<endl;
-    }*/
+    cout << "FIN"; //comprobación de que todos los procesos han acabado correctamente y con ello el programa completo
+   
     return 0;
 }
