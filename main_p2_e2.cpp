139c5e3891b2cf8d6a23f075f436c694019478d2
Miguel Aréjula Aisa <arejula10@gmail.com>
2022-10-05 17:08:35 +0200

    archivos

---
+++ main_p2_e2.cpp
@@ -0,0 +1,21 @@
+/* ----------------------------------------------------------------------------
+* File:   practica_1_V1.cpp
+* Author: PSCD-Unizar
+* Date:   septiembre 2020
+* Coms:   Parte del práctica 1 de PSCD
+*         Compilar mediante
+*           g++ practica_1_V1.cpp -o practica_1_V1 -std=c++11 -pthread
+* ----------------------------------------------------------------------------- */
+
+#include <iostream>
+#include <thread>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+
+   
+    return 0;
+}
